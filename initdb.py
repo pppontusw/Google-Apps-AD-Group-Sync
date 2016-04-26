a3ae2036d774b47452f59fd2e840d906c218e880
Pontus Wiberg <pontus@pontusw.com>
2016-04-26 11:07:27 +0200

    minor changes

--- initdb.py
+++ initdb.py
@@ -3,39 +3,55 @@ import json
 
 db = TinyDB('db.json')
 
-db.insert_multiple([{
-    'gappslist': 'nordics@example.com',
-    'members': [
-    	{
-        'attribute': 'physicalDeliveryOfficeName',
-        'value': 'Denmark'
+def insertIntoDB():
+    db.insert_multiple([{
+        'gappslist': 'nordics@example.com',
+        'members': [
+            {
+            'attribute': 'physicalDeliveryOfficeName',
+            'value': 'Denmark'
+            },
+            {
+            'attribute': 'physicalDeliveryOfficeName',
+            'value': 'Sweden'
+            },
+            {
+            'attribute': 'physicalDeliveryOfficeName',
+            'value': 'Norway'
+            },
+            {
+            'attribute': 'physicalDeliveryOfficeName',
+            'value': 'Finland'
+            },
+        ],
+        'type': 'OR'
         },
         {
-        'attribute': 'physicalDeliveryOfficeName',
-        'value': 'Sweden'
-        },
-        {
-        'attribute': 'physicalDeliveryOfficeName',
-        'value': 'Norway'
-        },
-        {
-        'attribute': 'physicalDeliveryOfficeName',
-        'value': 'Finland'
-        },
-    ],
-    'type': 'OR'
-    },
-    {
-    'gappslist': 'IT-US@example.com',
-    'members': [
-    	{
-        'attribute': 'physicalDeliveryOfficeName',
-        'value': 'USA'
-        },
-        {
-        'attribute': 'Department',
-        'value': 'IT'
-        },
-    ],
-    'type': 'AND'
-}])
+        'gappslist': 'IT-US@example.com',
+        'members': [
+            {
+            'attribute': 'physicalDeliveryOfficeName',
+            'value': 'USA'
+            },
+            {
+            'attribute': 'Department',
+            'value': 'IT'
+            },
+        ],
+        'type': 'AND'
+    }])
+
+def main():
+    selection = 0
+    while (selection != 'YES') and (selection != 'NO'):
+        selection = raw_input('Are you sure you want to re-initialize the database? THIS WILL REMOVE ALL YOUR CONFIGURED SYNCHRONIZATIONS AND REPLACE THEM WITH WHATEVER IS DEFINED IN INITDB.PY. Yes / No?').upper()
+    if (selection == 'YES'):
+        db.purge()
+        insertIntoDB()
+        print('OK!')
+        exit(0)
+    else:
+        exit(0)
+
+if __name__ == '__main__':
+    main()
--- purgedb.py
+++ purgedb.py
@@ -3,14 +3,18 @@ import json
 
 db = TinyDB('db.json')
 
-selection = 0
 
-while (selection != 'YES') and (selection != 'NO'):
-	selection = raw_input('Are you sure you want to purge the database? THIS WILL REMOVE ALL YOUR CONFIGURED SYNCHRONIZATIONS AND FILTERS. Yes / No?').upper()
 
-if (selection == 'YES'):
-	db.purge()
-	print('OK!')
-	exit(0)
-else:
-	exit(0)
+def main():
+	selection = 0
+	while (selection != 'YES') and (selection != 'NO'):
+		selection = raw_input('Are you sure you want to purge the database? THIS WILL REMOVE ALL YOUR CONFIGURED SYNCHRONIZATIONS AND FILTERS. Yes / No?').upper()
+	if (selection == 'YES'):
+		db.purge()
+		print('OK!')
+		exit(0)
+	else:
+		exit(0)
+
+if __name__ == '__main__':
+    main()
